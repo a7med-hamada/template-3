ef945210481dfc141d2535ce5db5afbb523ff1d5
ENG_A7MD <ahmed4569hamada@gmail.com>
2025-05-04 19:25:13 +0300

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        extend: {
+            screens: {
+                'custom': '1000px', // شاشة مخصصة تبدأ من 1000 بكسل
+            },
+        },
+    },
+}
